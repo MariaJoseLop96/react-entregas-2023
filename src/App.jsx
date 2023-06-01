2d97837eedbd645fd66378df49a7a61ad90cb703
maria jose <majoloperfido@hotmail.com>
2023-06-01 15:50:10 -0300

    error 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import ItemDetailContainer from './components/ItemDetailContainer';
+//import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
-    <Header /> 
-   <NavBar/>
-   <ItemListContainer/>
-   <ItemDetailContainer />
-   <Footer />
-   
-   
+      <BrowserRouter>
+      <Header /> 
+      <NavBar/>
+      <Routes>
+        <Route path={"/"} element={<ItemListContainer/>} /> 
+        <Route path={"/categoria/id"} element={<ItemListContainer />} /> 
+        {/* <ItemDetailContainer /> */}
+      </Routes>
+      <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Error404.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1>Error404</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Error404
