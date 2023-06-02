af5965c661a29b0cb2a5327039c870824a840c04
maria jose <majoloperfido@hotmail.com>
2023-06-02 16:46:25 -0300

    link a item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 // componente a desarrollar -mostrar info producto
 const Item = ({item}) => {
   return (
     <>
           <div class="card">
+            <Link to={"/item/" + item.id} className='text-dark text-decoration-none'>
                <img src={item.imagen} class="card-img-top" alt={item.titulo}/>
-              <div class="card-body">
+               <div class="card-body">
                 <h3>{item.titulo}</h3>
                <p class="card-text">${item.precio}</p>
               </div>
+            </Link>
          </div>
 
 
