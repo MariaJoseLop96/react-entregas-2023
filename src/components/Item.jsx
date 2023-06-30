835742181ccf782a20df021acd6ee16502b6507e
Maria Jose Loperfido <majoloperfido@hotmail.com>
2023-06-30 20:42:40 -0300

    se resolvio lo del item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,11 +6,11 @@ const Item = ({item}) => {
   return (
     <>
           <div class="card">
-            <Link to={"/item/" + item.idx} className='text-dark text-decoration-none'>
+            <Link to={"/item/" + item.id} className='text-dark text-decoration-none'>
                <img src={item.imagen} class="card-img-top" alt={item.titulo}/>
-               <div class="card-body">
+               <div className="card-body">
                 <h3>{item.titulo}</h3>
-               <p class="card-text">${item.precio}</p>
+               <p className="card-text">${item.precio}</p>
               </div>
             </Link>
          </div>
